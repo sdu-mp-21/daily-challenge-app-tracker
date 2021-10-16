2e8caa62efd6e4faa1e7f68af5c024902fc220db
Parasat <pashazhangbyrbay@gmail.com>
2021-10-16 15:11:14 +0600

    design only

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'main_page.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   static const String _title = '21 days Challenge';
+
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
