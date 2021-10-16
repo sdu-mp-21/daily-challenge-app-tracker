b9996a6c66c75f70c304cdc9e94125f2524b8e12
SSurikkiller <190107046@stu.sdu.edu.kz>
2021-10-16 22:43:58 +0600

    Just first try.

---
+++ lib/profile_class.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+main() => runApp(
+  Directionality(
+    textDirection: TextDirection.ltr,
+    child: Center(
+        child: MyMainWidget()
+    ),
+  ),
+);
+
+class MyMainWidget extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return Directionality(
+      textDirection: TextDirection.ltr,
+      child: Center (
+        child: BigColumn(),
+        //color: Colors.blue,
+        //padding: EdgeInsets.all(20),
+      ),
+    );
+  }
+
+}
+
+class BigColumn extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) { // [context] будет описан позже
+    return  Directionality(
+      textDirection: TextDirection.ltr,
+      child: Center (
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Imagination(),
+              SmallColumn(),
+              NumbRow(),
+            ]
+        ),
+        //color: Colors.blue,
+        //padding: EdgeInsets.all(20),
+      ),
+    );
+
+  }
+}
+
+
+
+class Imagination extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) { // [context] будет описан позже
+    return Center(
+      child: Expanded(
+        child: FittedBox(
+          fit: BoxFit.contain, // otherwise the logo will be tiny
+          child: FlutterLogo(),
+        ),
+      ),
+    );
+  }
+}
+
+//Numb not all
+class NumbRow extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) { // [context] будет описан позже
+    return Row(
+      children:const <Widget>[
+        Expanded(
+          child: Text('Challenges today: \n 0', textAlign: TextAlign.center),
+        ),
+        Expanded(
+          child: Text('Habits today: \n 0', textAlign: TextAlign.center),
+        ),
+        Expanded(
+          child: FittedBox(
+            fit: BoxFit.contain, // otherwise the logo will be tiny
+            child: FlutterLogo(),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class SmallColumn extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) { // [context] будет описан позже
+    return Center(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: const <Widget>[
+          Text('NickName: SSurikkiller', textAlign: TextAlign.center, style: TextStyle(fontStyle: FontStyle.italic)),
+          Text('ID: 190107046', textAlign: TextAlign.center,style: TextStyle(fontStyle: FontStyle.italic)),
+          Text('Email: SSurikkiller@mail.ru',textAlign: TextAlign.center,style: TextStyle(fontStyle: FontStyle.italic))
+        ],
+      ),
+    );
+  }
+}
