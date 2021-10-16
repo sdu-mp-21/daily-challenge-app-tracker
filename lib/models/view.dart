ce09d8e94f69adba3a63b74fe1c892677dd210d1
Daryn0001 <53093184+Daryn0001@users.noreply.github.com>
2021-10-17 01:11:05 +0600

    add class feedcreator

--- lib/models/view.dart
+++ lib/models/view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class Views extends StatelessWidget {
+  const Views({Key? key}) : super(key: key);
+
 
   @override
   Widget build(BuildContext context) {
