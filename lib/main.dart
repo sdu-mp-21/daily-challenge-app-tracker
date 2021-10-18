98d58eab64545468acedd0c2be08dd4dcdc7993d
mkaisarovich <73484960+mkaisarovich@users.noreply.github.com>
2021-10-19 05:57:46 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'splash_page.dart';
+import 'dart:async';
+
 
 void main() {
   runApp(MyApp());
@@ -20,3 +21,160 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+
+
+class SplashPage extends StatefulWidget {
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(const Duration(milliseconds: 4000), () {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => LoginPage()));
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              colors: [orangeColors, orangeLightColors],
+              end: Alignment.bottomCenter,
+              begin: Alignment.topCenter),
+        ),
+        child: Center(
+          child: Image.asset("logo.png"),
+        ),
+      ),
+    );
+  }
+}
+
+
+Color orangeColors = Color(0xffF5591F);
+Color orangeLightColors = Color(0xffF2861E);
+
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.only(bottom: 30),
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              flex: 1,
+              child: Container(
+                margin: EdgeInsets.only(left: 20, right: 20, top: 30),
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    _textInput(hint: "Email", icon: Icons.email),
+                    _textInput(hint: "Password", icon: Icons.vpn_key),
+                    Container(
+                      margin: EdgeInsets.only(top: 10),
+                      alignment: Alignment.centerRight,
+                      child: Text(
+                        "Forgot Password?",
+                      ),
+                    ),
+                    Expanded(
+                      child: Center(
+                        child: ButtonWidget(btnText: '',),
+                      ),
+                    ),
+                    RichText(
+                      text: TextSpan(children: [
+                        TextSpan(
+                            text: "Don't have an account ? ",
+                            style: TextStyle(color: Colors.black)),
+                        TextSpan(
+                            text: "Registor",
+                            style: TextStyle(color: orangeColors)),
+                      ]),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _textInput({controller, hint, icon}) {
+    return Container(
+      margin: EdgeInsets.only(top: 10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+        color: Colors.white,
+      ),
+      padding: EdgeInsets.only(left: 10),
+      child: TextFormField(
+        controller: controller,
+        decoration: InputDecoration(
+          border: InputBorder.none,
+          hintText: hint,
+          prefixIcon: Icon(icon),
+        ),
+      ),
+    );
+  }
+}
+
+
+class ButtonWidget extends StatelessWidget {
+  var btnText ="";
+  var onClick;
+
+
+  ButtonWidget({required this.btnText, this.onClick});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onClick,
+      child: Container(
+        width: double.infinity,
+        height: 40,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              colors: [orangeColors, orangeLightColors],
+              end: Alignment.centerLeft,
+              begin: Alignment.centerRight),
+          borderRadius: BorderRadius.all(
+            Radius.circular(100),
+          ),
+        ),
+        alignment: Alignment.center,
+        child: Text(
+          btnText,
+          style: TextStyle(
+              fontSize: 20,
+              color: Colors.white,
+              fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
+
+
