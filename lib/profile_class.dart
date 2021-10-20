8a51e7594b35973d270938ae8c7bca8e6887d881
SSurikkiller <190107046@stu.sdu.edu.kz>
2021-10-20 14:50:44 +0600

    Just deleted debug.

--- lib/profile_class.dart
+++ lib/profile_class.dart
@@ -8,6 +8,7 @@ class StaticApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: MyMainWidget(),
     );
   }
