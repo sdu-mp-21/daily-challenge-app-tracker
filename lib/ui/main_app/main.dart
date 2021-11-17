3a8b2c94eeb6c9d22cbcf33b6bd235af606b3526
SSurikkiller <190107046@stu.sdu.edu.kz>
2021-11-17 15:41:38 +0600

    I added my part

--- lib/ui/main_app/main.dart
+++ lib/ui/main_app/main.dart
@@ -2,6 +2,8 @@
 import 'package:challenge_tracker/ui/pages/auth/auth_page.dart';
 import 'package:challenge_tracker/ui/pages/auth/login_widget.dart';
 import 'package:challenge_tracker/ui/pages/auth/privacy_policy.dart';
+import 'package:challenge_tracker/ui/pages/auth/profile_class.dart';
+import 'package:challenge_tracker/ui/pages/auth/settings_class.dart';
 import './navigator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
@@ -25,6 +27,8 @@ class MainApp extends StatelessWidget {
         '/register': (context) =>  const Authorization(),
         '/login': (context) => const LoginPage(),
         '/privacyPolicy': (context) => const PrivacyPolicy(),
+        '/settings': (context) => const SettingsPage(),
+        '/profile': (context) => const ProfilePage(),
         //'/createChallenge': (context) => const CreateNewWidget(page: 0),
         //'/createPost': (context) => const CreateNewWidget(page: 1)
       },
--- lib/ui/pages/auth/registration_widget.dart
+++ lib/ui/pages/auth/registration_widget.dart
@@ -1,6 +1,7 @@
 import 'package:challenge_tracker/ui/pages/auth/auth_page.dart';
-import 'package:challenge_tracker/ui/widgets/buttons.dart';
+// import 'package:challenge_tracker/ui/widgets/buttons.dart';
 import 'package:challenge_tracker/ui/widgets/color_custom.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
@@ -12,50 +13,210 @@ class RegPage extends StatefulWidget {
 }
 
 class _RegPageState extends State<RegPage> {
+
+
+  late String _name;
+  late String _email;
+  late String _password;
+  // late String _passwordAgain;
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  Widget _buildName() {
+    return Container(
+      margin: const EdgeInsets.only(top: 20),
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+        color: Colors.white,
+      ),
+      padding: const EdgeInsets.only(left: 40,right: 40),
+      child:TextFormField(
+        decoration: InputDecoration(labelText: 'Name',icon: new Icon(Icons.person)),
+        maxLength: 16,
+        validator: (value) {
+          if (value!.isEmpty) {
+            return 'Name is Required';
+          }
+
+          return null;
+        },
+        onSaved: (value) {
+          _name = value!;
+        },
+      ),
+    );
+
+  }
+
+  Widget _buildEmail() {
+    return Container(
+      margin: const EdgeInsets.only(top: 20),
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+        color: Colors.white,
+      ),
+      padding: const EdgeInsets.only(left: 40,right: 40),
+      child: TextFormField(
+        decoration: InputDecoration(labelText: 'Email',icon: new Icon(Icons.email)),
+        validator: (value) {
+          if (value!.isEmpty) {
+            return 'Email is Required';
+          }
+          if (!RegExp(
+              r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+              .hasMatch(value)) {
+            return 'Please enter a valid email Address';
+          }
+          return null;
+        },
+        onSaved: (value) {
+          _email = value!;
+        },
+      ),
+    );
+  }
+
+  Widget _buildPassword() {
+    return Container(
+      margin: const EdgeInsets.only(top: 20),
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+        color: Colors.white,
+      ),
+      padding: const EdgeInsets.only(left: 40,right: 40),
+      child:TextFormField(
+        decoration: InputDecoration(labelText: 'Password',icon: new Icon(Icons.vpn_key)),
+        keyboardType: TextInputType.visiblePassword,
+        validator: (value) {
+          if (value!.isEmpty) {
+            return 'Password is Required';
+          }else if(value.length < 6){
+            return 'Password must be more than 6 characters';
+          }
+
+          return null;
+        },
+        onSaved: (value) {
+          _password = value!;
+        },
+      ),
+    );
+  }
+
+
+
+  Widget _buildPasswordAgain() {
+    return Container(
+      margin: const EdgeInsets.only(top: 20),
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+        color: Colors.white,
+      ),
+      padding: const EdgeInsets.only(left: 40,right: 40),
+      child:TextFormField(
+        decoration: InputDecoration(labelText: 'Password',icon: new Icon(Icons.vpn_key)),
+        keyboardType: TextInputType.visiblePassword,
+        validator: (value) {
+          if (value!.isEmpty) {
+            return 'Password is Required';
+          }else if(value.length < 6){
+            return 'Password must be more than 6 characters';
+          }
+          else if(value != _password){
+            return 'The password does not match the pre-password';
+          }
+
+          return null;
+        },
+        // onSaved: (value) {
+        //   _password = value!;
+        // },
+      ),
+    );
+  }
+
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
 
-        toolbarHeight: 150,
+        toolbarHeight:130,
         flexibleSpace: Authorization.getHeader(),
       ),
 
       body: Container(
         padding: const EdgeInsets.only(bottom: 30),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: <Widget>[
             Expanded(
-              child: Container(
-                margin: const EdgeInsets.only(left: 60, right: 60, top: 30),
+              child: Form(
+                // margin: const EdgeInsets.only(left: 60, right: 60, top: 30),
+                key: _formKey ,
                 child: Column(
                   children: <Widget>[
-                    _textInput(hint: "Full name", icon: Icons.person),
-                    _textInput(hint: "Email", icon: Icons.email),
-                    _textInput(hint: "Phone Number", icon: Icons.call),
-                    _textInput(hint: "Password", icon: Icons.vpn_key),
-                    Expanded(
-                      child: Center(
-                        child: Buttons.getAuthButton(
-                          "REGISTER", (){
-                            Navigator.pop(context);
-                          },
+                    _buildName(),
+                    _buildEmail(),
+                    _buildPassword(),
+                    _buildPasswordAgain(),
+                    const SizedBox(height: 40),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20.0,bottom:20,left: 20,right: 20),
+
+                      child: ElevatedButton(
+
+
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            return;
+                          }
+                          _formKey.currentState!.save();
+                          print(_name);
+                          print(_email);
+                          print(_password);
+
+
+                          //Send to API
+                        },
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(ColorsCustom.orangeColors),
                         ),
+                        child: Container(
+                          height: 40,
+                          color: ColorsCustom.orangeColors,
+                          alignment: Alignment.center,
+                          child: const Text('Register', style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold, letterSpacing: 2),),
+                        ),
+
                       ),
                     ),
+
+
+
+
+
+
+
                     RichText(
+
                       text: TextSpan(children: [
                         const TextSpan(
                             text: "Already a member ? ",
                             style: TextStyle(color: Colors.black,
                                 letterSpacing: 2)),
                         TextSpan(
-                            text: "Login",
-                            style: TextStyle(color: ColorsCustom.orangeColors),
-                            recognizer: TapGestureRecognizer()
+                          text: "Login",
+                          style: TextStyle(color: ColorsCustom.orangeColors),
+                          recognizer: TapGestureRecognizer()
                             ..onTap = () {
-                                    Navigator.pushNamed(context, '/login');
-                                },
+                              Navigator.pushNamed(context, '/login');
+                            },
                         ),
                       ]),
                     )
@@ -70,21 +231,5 @@ class _RegPageState extends State<RegPage> {
   }
 
 
-  Widget _textInput({hint, icon}) {
-    return Container(
-      margin: const EdgeInsets.only(top: 20),
-      decoration: const BoxDecoration(
-        borderRadius: BorderRadius.all(Radius.circular(40)),
-        color: Colors.white,
-      ),
-      padding: const EdgeInsets.only(left: 20),
-      child: TextFormField(
-        decoration: InputDecoration(
-          border: const OutlineInputBorder(),
-          hintText: hint,
-          prefixIcon: Icon(icon),
-        ),
-      ),
-    );
-  }
 }
+
