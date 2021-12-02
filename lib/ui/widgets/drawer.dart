8b469743b9f64ebbf910ecffb932aa061c96cd78
Parasat <pashazhangbyrbay@gmail.com>
2021-12-02 11:59:09 +0600

    image picker in progress

--- lib/ui/widgets/drawer.dart
+++ lib/ui/widgets/drawer.dart
@@ -16,13 +16,8 @@ class OurDrawer extends StatelessWidget {
         children: <Widget>[
           GestureDetector(
           onTap: () => user == null ? Navigator.pushNamed(context, '/login') : Navigator.pushNamed(context, '/profile'),
-          child:DrawerHeader(
-            decoration: const BoxDecoration(
-              color: Colors.blue,
-            ),
-            child:user != null ? authorized(user!) : nonAuthorized(context),
-
-          )),
+          child:user != null ? authorized(user!, context) : nonAuthorized(context),
+             ),
           ListTile(
             leading: const Icon(Icons.privacy_tip),
             title: const Text('Privacy Policy'),
@@ -37,53 +32,177 @@ class OurDrawer extends StatelessWidget {
       ),
     );
   }
-  Widget authorized(User user) {
-    return Stack(
-      children: [
-        Align(
-          alignment: Alignment.centerLeft,
-          child: (user.photoURL != null) ?  CircleAvatar(
-            backgroundImage: NetworkImage(user.photoURL.toString()),
-            radius: 50.0,
-          ) : const Icon(Icons.account_circle_sharp, size: 100,),
-        ),
-        Align(
-          alignment: Alignment.centerRight + const Alignment(0, -.2),
-          child: Text(
-            user.displayName.toString(),
-            style: const TextStyle(color: Colors.white, fontSize: 20.0),
-          ),
-        ),
-        Align(
-          alignment: Alignment.centerRight + const Alignment(0, .1),
-          child: Text(
-            user.email.toString(),
-            style: const TextStyle(
-                color: Colors.white70,
-                fontSize: 11
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-  Widget nonAuthorized(BuildContext context) {
-    return  Stack(
-          children: [
-            const Align(
-              alignment: Alignment.centerLeft,
-              child: Icon(Icons.account_circle_sharp, size: 100,),
-            ),
-            Align(
-              alignment: Alignment.centerRight + const Alignment(0, -.1),
-              child: Text(
-                'login/register'.toUpperCase(),
-                style: const TextStyle(color: Colors.white, fontSize: 18.0),
+  Widget authorized(User user, BuildContext context) {
+    return DrawerHeader(
+      decoration: const BoxDecoration(
+        color: Colors.blue,
+      ),
+      child: Stack(
+        children: [
+      // const Align(
+      //   alignment: Alignment.centerLeft,
+      //   child: Icon(Icons.account_circle_sharp, size: 100,)
+      //   ),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: (user.photoURL != null) ?  CircleAvatar(
+                backgroundImage: NetworkImage(user.photoURL.toString()),
+                radius: 50.0,
+              )
+                : GestureDetector(
+                onTap: () {
+                  _showSelectionDialog(context);
+                  },
+                  child: const Icon(Icons.account_circle_sharp, size: 100,),
+                  ),
+                  ),
+                  Align(
+                  alignment: Alignment.centerRight + const Alignment(0, -.2),
+                  child: Text(
+                  user.displayName.toString(),
+                  style: const TextStyle(color: Colors.white, fontSize: 20.0),
+                  ),
+                  ),
+                  Align(
+                  alignment: Alignment.centerRight + const Alignment(0, .1),
+                  child: Text(
+                  user.email.toString(), style: const TextStyle(
+                  color: Colors.white70,
+                  fontSize: 11
               ),
             ),
-          ],
-        );
+          ),
+        ],
+      ),
+
+    );
+
   }
+  Widget nonAuthorized(BuildContext context) {
+    return DrawerHeader(
+      decoration: const BoxDecoration(
+        color: Colors.blue,
+      ),
+      child:Stack(
+        children: [
+          const Align(
+            alignment: Alignment.centerLeft,
+            child: Icon(Icons.account_circle_sharp, size: 100,),
+          ),
+          Align(
+            alignment: Alignment.centerRight + const Alignment(0, -.1),
+            child: Text(
+              'login/register'.toUpperCase(),
+              style: const TextStyle(color: Colors.white, fontSize: 18.0),
+            ),
+          ),
+        ],
+      ),
+
+    );
+
+  }
+
+  _showSelectionDialog(BuildContext context) {
+
+    showModalBottomSheet(
+        backgroundColor: Colors.black87,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0),),
+        context: context,
+        builder: (BuildContext context) {
+        return Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+              child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const SizedBox(
+                      height: 30,
+                      child: Text('Select Source', style: TextStyle(color: Colors.white, fontSize: 22.0)),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        GestureDetector(
+                          onTap: () {Navigator.pop(context);},
+                          child: const Icon(Icons.collections_outlined, color: Colors.white, size: 40,),
+                        ),
+                        GestureDetector(
+                          onTap: () {Navigator.pop(context);},
+                          child: const Icon(Icons.camera_alt_rounded, color: Colors.white, size: 40,),
+                        )
+                      ],
+                    )
+                  ]
+              ),
+
+        );
+    });
+
+
+    // ScaffoldMessenger.of(context).showSnackBar(
+    //   SnackBar(
+    //       padding: const EdgeInsets.symmetric(
+    //         vertical: 8,
+    //         horizontal: 8.0, // Inner padding for SnackBar content.
+    //       ),
+    //       behavior: SnackBarBehavior.floating,
+    //       shape: RoundedRectangleBorder(
+    //         borderRadius: BorderRadius.circular(10.0),
+    //       ),
+    //     content: Column(
+    //       mainAxisSize: MainAxisSize.min,
+    //         children: [
+    //           const SizedBox(
+    //             height: 20,
+    //             child: Text('Select Source'),
+    //           ),
+    //           Row(
+    //           children: [
+    //             GestureDetector(
+    //               onTap: () {Navigator.pop(context);},
+    //               child: const Icon(Icons.collections_outlined, color: Colors.white, size: 30,),
+    //             ),
+    //             GestureDetector(
+    //               onTap: () {Navigator.pop(context);},
+    //               child: const Icon(Icons.camera_alt_rounded, color: Colors.white, size: 30,),
+    //             )
+    //           ],
+    //         )
+    // ]
+    //     )
+    //     ),
+    //
+    // );
+    // showDialog(
+    //   context: context,
+    //   builder: (BuildContext context) {
+    //     return SimpleDialog(
+    //         title: const Text('Select Source'),
+    //         children: <Widget>[
+    //           SimpleDialogOption(
+    //             child: const Icon(Icons.collections_outlined),
+    //             onPressed: () {
+    //               // selectOrTakePhoto(ImageSource.gallery);
+    //               Navigator.pop(context);
+    //               },
+    //           ),
+    //           SimpleDialogOption(
+    //             child: const Icon(Icons.camera_alt_rounded),
+    //             onPressed: () {
+    //               // selectOrTakePhoto(ImageSource.camera);
+    //               Navigator.pop(context);
+    //               },
+    //           ),
+    //         ]
+    //     );
+    //   },
+    // );
+  }
+
+  // selectOrTakePhoto(ImageSource imageSource) async {
+  //
+  // }
+
 }
 
 
